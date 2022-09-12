65708367e740a3f7fc69e5242cea14e5e45da0c8
Nicholas Igunbor <nicholas.igunbor@decagon.dev>
2022-09-12 22:20:09 +0100

    <"binary">

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -7,22 +7,26 @@
 
 
  function binaryReversal (num){
+
     let binaryNum = parseInt(num);
-    //first step is to convert the number to binary
+    
     var binary = binaryNum.toString(2);
    
     var padded = binary.padStart(8, '0');
     
     var reversed = padded.split('').reverse().join('');
+
     console.log(typeof reversed);
-    // converting the binary back into an integer
+    
     var result = parseInt(reversed, 2);
+
     console.log(typeof result);
+
     var finealR = result.toString();
+
     return finealR;
 
 }
 
-console.log(binaryReversal("100"));
 
 module.exports = binaryReversal;
