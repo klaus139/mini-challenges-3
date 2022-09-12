d4dbf63429615f740c01ba556cb97c26bd89ffc3
Nicholas Igunbor <nicholas.igunbor@decagon.dev>
2022-09-12 21:11:06 +0100

    <"list sorting">

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,26 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+
+
+
+ function binaryReversal (num){
+    let binaryNum = parseInt(num);
+    //first step is to convert the number to binary
+    var binary = binaryNum.toString(2);
+   
+    var padded = binary.padStart(8, '0');
+    
+    var reversed = padded.split('').reverse().join('');
+    console.log(typeof reversed);
+    // converting the binary back into an integer
+    var result = parseInt(reversed, 2);
+    console.log(typeof result);
+    var finealR = result.toString();
+    return finealR;
+
+}
+
+console.log(binaryReversal("100"));
 
 module.exports = binaryReversal;
--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,63 @@
-function listSorting(needle, haystack) {}
 
+
+
+function listSorting(needle, haystack) {
+    // if the haystack is empty, return -1
+    if (haystack.length === 0){
+        return -1;
+    } 
+    // occurence of the needle in the haystack
+    let occurence = 0;
+    // loop through the haystack
+    for (let i = 0; i < haystack.length; i++){
+            // if the needle is found, increment occurence
+        if (haystack[i] === needle){
+            occurence++;
+        }
+    }
+     
+    //check if haystack is an array of arrays
+    if(typeof(haystack[0]) === 'object'){
+        let column;
+        let row;
+        // loop through the haystack
+        for (let i = 0; i < haystack.length; i++){
+            // loop through the subarrays
+            for (let j = 0; j < haystack[i].length; j++){
+                if (haystack[i][j] === needle){
+                    column = haystack[i].lastIndexOf(needle);
+                    row = i;
+                }
+            }
+        
+        }
+    
+        //check if the needle is in any row and return the row and column
+        if (row == undefined ) return -1;
+        return [row, column];
+        } else {
+            return haystack.lastIndexOf(needle);
+        }
+    
+
+    // if the occurence is 0, return -1
+    if (occurence === 0){
+        return -1;
+    } else if (occurence === 1){
+        // if the occurence is 1, return the index of the needle
+        return haystack.indexOf(needle);
+    } else {
+        //if the occurence is more than 1, return the index of the last occurence
+        return haystack.lastIndexOf(needle);   
+    
+    }  
+}
+console.log(listSorting(200, [
+    [100, 200, 300],
+    [300, 200, 100],
+    [300, 100, 200],
+    [200, 100, 300],
+    [100, 200, 300],
+  ]));
+        
 module.exports = listSorting;
